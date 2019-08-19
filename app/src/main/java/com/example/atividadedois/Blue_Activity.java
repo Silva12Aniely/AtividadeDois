debb142a75cf72d6b5f51cb63a35197f607be751
aniely.osilva <anielysilva08@gmail.com>
2019-08-19 17:20:30 -0300

    19/08 - Up

---
+++ app/src/main/java/com/example/atividadedois/Blue_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atividadedois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Blue_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.blue_layout);
+    }
+}

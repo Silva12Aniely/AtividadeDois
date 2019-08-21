68b6c5ec51c1efbde1b53044cd19d850ee18eb0c
aniely <anielysilva08@gmail.com>
2019-08-20 22:45:59 -0300

    20/08 - 22:45 - Home

---
+++ app/src/main/java/com/example/atividadedois/Diamond_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atividadedois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Diamond_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.diamond_layout);
+    }
+}

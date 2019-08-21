8d08781ffadf75fc2b0f2aba213254ab853bcfc7
Aniely <anielysilva08@gmail.com>
2019-08-21 12:25:31 -0300

    Well Done!

---
+++ app/src/main/java/com/example/atividadedois/Login_Zad_Activity.java
@@ -0,0 +1,14 @@
+package com.example.atividadedois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login_Zad_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login__zad_layout);
+    }
+}
